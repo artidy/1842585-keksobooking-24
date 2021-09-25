d19af67901137ba02a80e9ae192501e9260e0501
Andrey Pechennikov <ap@devandy.me>
2021-09-26 00:03:34 +0600

    Вынес вычисление рандомного числа в отдельную функцию

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 /**
+ * Вспомогательная функция для вычисления случайного числа в диапазоне.
+ * Не надо пользоваться этой функцией напрямую.
+ *
+ * @param number min - нижняя граница диапазона.
+ * @param number max - верхняя граница диапазона.
+ * @return number - случайное число из диапазона min <-> max
+ */
+const getRandomNumber = (min, max) => Math.random() * (max - min) + min;
+
+/**
  * Генерирует случайное число в переданном диапазоне включительно.
  * Если минимальное значение диапазона меньше максимального они поменяются местами.
  *
@@ -14,19 +24,16 @@
  * @param number decimals - количество чисел после запятой
  * @return number - случайное число из диапазона min <-> max
  */
-const generateRandomNumber = (min, max, decimals = 0) => {
+const generateRandomNumber = (min = 0, max = 0, decimals = 0) => {
   if (min < 0 ||
     max < 0 ||
     decimals < 0) {
     throw new Error('Значения не могут быть меньше 0');
   }
 
-  if (max < min) {
-    const change = max;
-    max = min;
-    min = change;
-  }
-  return (Math.random() * (max - min) + min).toFixed(decimals);
+  const random = max > min ? getRandomNumber(min, max) : getRandomNumber(max, min);
+
+  return random.toFixed(decimals);
 };
 
 generateRandomNumber(1, 1, 7);
