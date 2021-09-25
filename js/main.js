c72efc870ce8a3baa9c0c6385448c6542b223669
Andrey Pechennikov <ap@devandy.me>
2021-09-25 21:18:55 +0600

    Добавил описание к функции

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
+/**
+ * Генерирует случайное число в переданном диапазоне включительно.
+ * Если минимальное значение диапазона меньше максимального они поменяются местами.
+ *
+ * Примеры:
+ * 1) generateRandomNumber(1, 1, 7);
+ * result = 1.0000000
+ *
+ * 2) generateRandomNumber(3, 1);
+ * result = 2
+ *
+ * @param number min - нижняя граница диапазона.
+ * @param number max - верхняя граница диапазона.
+ * @param number decimals - количество чисел после запятой
+ * @return number - случайное число из диапазона min <-> max
+ */
 const generateRandomNumber = (min, max, decimals = 0) => {
-  if (min < 0 || max < 0) {
-    return 'Значения не могут быть меньше 0';
+  if (min < 0 ||
+    max < 0 ||
+    decimals < 0) {
+    throw new Error('Значения не могут быть меньше 0');
   }
 
   if (max < min) {
