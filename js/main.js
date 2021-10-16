1a36c4c082813e3b1256e3e16dffc1b0e9d5d28e
Andrey Pechennikov <ap@devandy.me>
2021-10-16 21:38:14 +0600

    Доработал функционал формы, добавил проверки для обязательных полей

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import generateAdvertisements from './data.js';
 import {createPopup} from './popup.js';
-import {activateForm, deactivateForm, findChildren} from './form.js';
+import './form.js';
 
 /**
  * Количество объявлений
@@ -14,18 +14,6 @@ const OFFER_COUNT = 10;
  * @type {number}
  * */
 const TARGET_OFFER = 5;
-/**
- * Имя класса для отключения формы
- * @constant
- * @type {string}
- * */
-const DISABLE_CLASS = 'ad-form--disabled';
-/**
- * Подчиненные элементы, которые необходимо обработать
- * @constant
- * @type {array<string>}
- * */
-const CONTROL_SELECTORS = ['select', 'fieldset'];
 
 const advertisements = generateAdvertisements(OFFER_COUNT);
 
@@ -34,13 +22,3 @@ const popupTemplate = document.querySelector('#card').content;
 
 const popup = createPopup(advertisements[TARGET_OFFER], popupTemplate, '.popup');
 canvas.append(popup);
-
-const addForm = document.querySelector('.ad-form');
-const addElements = findChildren(addForm, CONTROL_SELECTORS);
-deactivateForm(addForm, DISABLE_CLASS, addElements);
-activateForm(addForm, DISABLE_CLASS, addElements);
-
-const filterForm = document.querySelector('.map__filters');
-const filterElements = findChildren(filterForm, CONTROL_SELECTORS);
-deactivateForm(filterForm, DISABLE_CLASS, filterElements);
-activateForm(filterForm, DISABLE_CLASS, filterElements);
