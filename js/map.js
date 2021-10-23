35ae398912bfa97711bf4d6ac650599829ea5bd0
Andrey Pechennikov <ap@devandy.me>
2021-10-23 22:48:15 +0600

    Вынес константы по настройке карты в enum, добавил отступы между логическими блоками

--- js/map.js
+++ js/map.js
@@ -2,35 +2,44 @@ import generateAdvertisements from './data.js';
 import {activateForm, deactivateForm, formAdd, formAddChildren, formFilter, formFilterChildren} from './form.js';
 import {createPopup} from './popup.js';
 
+const MapSettings = {
+  ID: 'map-canvas',
+  ZOOM: 15,
+  ICON_WIDTH: 26,
+  ICON_HEIGHT: 52,
+  DEVIDER: 2,
+  START_LOCATION: {lat: 35.68500, lng: 139.75140},
+  TILE_MAP: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  MAIN_ICON: 'img/main-pin.svg',
+  OFFER_ICON: 'img/pin.svg',
+  POPUP_CLASS: '.popup',
+};
+
+const OFFER_COUNT = 10;
+const FIXED_VALUE = 5;
+
 deactivateForm(formFilter, formFilterChildren);
 deactivateForm(formAdd, formAddChildren);
 
-const ZOOM = 15;
-const ICON_WIDTH = 26;
-const ICON_HEIGHT = 52;
-const DEVIDER = 2;
-const OFFER_COUNT = 10;
-const START_LOCATION = {lat: 35.68500, lng: 139.75140};
-const TILE_MAP = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const FIXED_VALUE = 5;
 const offers = generateAdvertisements(OFFER_COUNT);
+
 const locationField = formAdd.querySelector('#address');
 const popupTemplate = document.querySelector('#card').content;
-const popupClass = '.popup';
-const map = L.map('map-canvas');
-L.tileLayer(TILE_MAP,{attribution: ATTRIBUTION}).addTo(map);
-const mainIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
-  iconSize: [ICON_WIDTH, ICON_HEIGHT],
-  iconAnchor: [ICON_WIDTH / DEVIDER, ICON_HEIGHT],
+
+const map = L.map(MapSettings.ID);
+
+L.tileLayer(MapSettings.TILE_MAP,{attribution: MapSettings.ATTRIBUTION}).addTo(map);
+
+const getIconSettings = (url) => ({
+  iconUrl: url,
+  iconSize: [MapSettings.ICON_WIDTH, MapSettings.ICON_HEIGHT],
+  iconAnchor: [MapSettings.ICON_WIDTH / MapSettings.DEVIDER, MapSettings.ICON_HEIGHT],
 });
-const offerIcon = L.icon({
-  iconUrl: '../img/pin.svg',
-  iconSize: [ICON_WIDTH, ICON_HEIGHT],
-  iconAnchor: [ICON_WIDTH / DEVIDER, ICON_HEIGHT],
-});
-const mainMarker = L.marker(START_LOCATION, {icon: mainIcon, draggable: true});
+
+const mainIcon = L.icon(getIconSettings(MapSettings.MAIN_ICON));
+const offerIcon = L.icon(getIconSettings(MapSettings.OFFER_ICON));
+const mainMarker = L.marker(MapSettings.START_LOCATION, {icon: mainIcon, draggable: true});
 
 const setLocation = (target) => {
   const buildingLocation = target.getLatLng();
@@ -49,13 +58,11 @@ mainMarker.on('moveend', onMarkerMoveend);
 mainMarker.addTo(map);
 
 offers.forEach((offer) => {
-  L.marker(offer.location, {
-    icon: offerIcon,
-    iconSize: [ICON_WIDTH, ICON_HEIGHT],
-    iconAnchor: [ICON_WIDTH / DEVIDER, ICON_HEIGHT],
-  }).addTo(map).bindPopup(createPopup(offer, popupTemplate, popupClass));
+  L.marker(offer.location, {icon: offerIcon})
+    .addTo(map)
+    .bindPopup(createPopup(offer, popupTemplate, MapSettings.POPUP_CLASS));
 });
 
-map.setView(START_LOCATION, ZOOM);
+map.setView(MapSettings.START_LOCATION, MapSettings.ZOOM);
 
 setLocation(mainMarker);
